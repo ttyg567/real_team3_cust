2a3d6eee05bb9278241cdcf33588e97f64577393
sungyoung1107 <dong09161107@gmail.com>
2023-06-22 14:29:47 +0900

    2023-06-22

--- src/main/java/com/kbstar/controller/CouponController.java
+++ src/main/java/com/kbstar/controller/CouponController.java
@@ -64,29 +64,29 @@ public class CouponController {
         return "redirect:/loginplz"; // 로그인 후 이용해주세요 !!!!!
     }
 
-    @RequestMapping("/my_coupon")
-    public String my_coupon(Model model, Integer custNo, HttpSession session, HttpServletRequest request) throws Exception {
-        List<Groupboard> list1 = null;
-        List<Groupboard> list2 = null;
-        // 로그인 체크
-//        Cust cust = (Cust) session.getAttribute("logincust");
-//        if (cust == null) {
-//            // 로그인 완료한 뒤 다시 돌아오도록 url 저장
-//            String redirectUrl = "/groupboard/my_applyjoin?custNo=" + custNo;
-//            request.getSession().setAttribute("redirectUrl", redirectUrl);
-//            return "redirect:/login"; // 로그인하지 않은 경우 로그인 페이지로 리다이렉트
+//    @RequestMapping("/my_coupon")
+//    public String my_coupon(Model model, Integer custNo, HttpSession session, HttpServletRequest request) throws Exception {
+//        List<Groupboard> list1 = null;
+//        List<Groupboard> list2 = null;
+//        // 로그인 체크
+////        Cust cust = (Cust) session.getAttribute("logincust");
+////        if (cust == null) {
+////            // 로그인 완료한 뒤 다시 돌아오도록 url 저장
+////            String redirectUrl = "/groupboard/my_applyjoin?custNo=" + custNo;
+////            request.getSession().setAttribute("redirectUrl", redirectUrl);
+////            return "redirect:/login"; // 로그인하지 않은 경우 로그인 페이지로 리다이렉트
+////        }
+//
+//        try {
+//            list1 = groupboardService.getMyJoin(custNo); // 고객의 조인 정보를 list에 담기.
+//            list2 = groupboardService.getMyCreateJoin(custNo);
+//
+//        } catch (Exception e) {
+//            throw new Exception(e);
 //        }
-
-        try {
-            list1 = groupboardService.getMyJoin(custNo); // 고객의 조인 정보를 list에 담기.
-            list2 = groupboardService.getMyCreateJoin(custNo);
-
-        } catch (Exception e) {
-            throw new Exception(e);
-        }
-        model.addAttribute("myapplyjoin", list1); // 내가 참여한 조인정보 보기
-        model.addAttribute("mycreatejoin", list2); // 내가 만든 조인정보 보기
-        model.addAttribute("center", dir + "myjoin");
-        return "index"; // 로그인 후 "my_applyjoin" 페이지로 이동
-    }
+//        model.addAttribute("myapplyjoin", list1); // 내가 참여한 조인정보 보기
+//        model.addAttribute("mycreatejoin", list2); // 내가 만든 조인정보 보기
+//        model.addAttribute("center", dir + "myjoin");
+//        return "index"; // 로그인 후 "my_applyjoin" 페이지로 이동
+//    }
 }
