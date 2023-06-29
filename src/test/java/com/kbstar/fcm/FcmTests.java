cf8145530ee69daf7e8e28abc68eaf7bb28c240a
sungyoung1107 <dong09161107@gmail.com>
2023-06-29 09:07:47 +0900

    2023-06-29

--- src/test/java/com/kbstar/fcm/FcmTests.java
+++ src/test/java/com/kbstar/fcm/FcmTests.java
@@ -29,7 +29,7 @@ class FcmTests {
 //        pushNotificationUtil.sendCommonMessage("SPRING", "SPRING", "/cust/register");
 //        pushNotificationUtil.sendCommonMessage("Open Coupon Box", "Open Coupon Box", "/coupon/show?couponNo="+24, "fRWwsGIRRw2U-tuhPYwng4:APA91bHsla7S9VBvhhVdU_SGGrMPMiTw47hQ6Fd77eGQz6XyyAsXVjndxJMjdcIHk-ayeb8k-8P8RDY5PTo7_t5QwhRNk6llDK2envljb7JFbdMqbmsXenJR5vGaWZNvLfd0G-d9afXj");
 //        pushNotificationUtil.sendCommonMessage("Open Coupon Box", "Open Coupon Box", "/coupon/show?couponNo="+25,  "eLTC8rfpQqeWLE1DDs8dY1:APA91bFdqm7IK3qc22AKpiemV5f1lOQ30NCcbgXDNY7uuZ9Y1vtzgTvThPngOKhxlKoYFSl4JhjT_FfAjVF3PQPLut90aWHYxl8YcFAW6D4xtCyoaVvqKY-oIRvjlWwsT9lWst4ys4OQ");
-//        pushNotificationUtil.sendCommonMessage("Open Coupon Box", "Open Coupon Box", "/cust/login",  "eLTC8rfpQqeWLE1DDs8dY1:APA91bFdqm7IK3qc22AKpiemV5f1lOQ30NCcbgXDNY7uuZ9Y1vtzgTvThPngOKhxlKoYFSl4JhjT_FfAjVF3PQPLut90aWHYxl8YcFAW6D4xtCyoaVvqKY-oIRvjlWwsT9lWst4ys4OQ");
+        pushNotificationUtil.sendCommonMessage("Open Coupon Box", "Open Coupon Box", "/cust/login",  "eLTC8rfpQqeWLE1DDs8dY1:APA91bFdqm7IK3qc22AKpiemV5f1lOQ30NCcbgXDNY7uuZ9Y1vtzgTvThPngOKhxlKoYFSl4JhjT_FfAjVF3PQPLut90aWHYxl8YcFAW6D4xtCyoaVvqKY-oIRvjlWwsT9lWst4ys4OQ");
     }
 
     // https://115.85.183.166/coupon/received?couponNo=25
